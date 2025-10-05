8b808b6f0f85efa270551dbc3ab7662d0a4e0bed
Ghqlq <gb2789@nyu.edu>
2025-10-05 19:56:47 -0400

    register_Ghala

--- register_file.v
+++ register_file.v
@@ -1,7 +1,7 @@
 module register_file (
     input [1:0] MUX_tgt, //          00:mem_out      01:alu_out      10:pc+1
     input MUX_rf, WE_rf, clk, //     MUX_rf -> 0:rC(ADD/NAND)  1:rA(SW/BEQ)
-    input [15:0] mem_out, alu_out, pc, instruction
+    input [15:0] mem_out, alu_out, pc, instruction,
 
     output [15:0] reg_out1, reg_out2
 );
@@ -13,8 +13,8 @@ module register_file (
 
     assign reg_out1  = register_file[rB];
     assign reg_out2  =  (MUX_rf == 1'b0) ? register_file[rC] : register_file[rA]; 
-    assign [15:0] MUX_output = (MUX_tgt == 2'b00) ? MEM_out :
-                               (MUX_tgt == 2'b01) ? ALU_out :
+    assign [15:0] MUX_output = (MUX_tgt == 2'b00) ? mem_out :
+                               (MUX_tgt == 2'b01) ? alu_out :
                                (MUX_tgt == 2'b10) ? pc + 16'd1;
 
     always @(posedge clk) begin
