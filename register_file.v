ca66813fc7b7b8b8e050929e1a440dc09e8f1616
Ghqlq <gb2789@nyu.edu>
2025-10-05 20:05:06 -0400

    register_Ghala

--- register_file.v
+++ register_file.v
@@ -13,12 +13,20 @@ module register_file (
 
     assign reg_out1  = register_file[rB];
     assign reg_out2  =  (MUX_rf == 1'b0) ? register_file[rC] : register_file[rA]; 
-    assign [15:0] MUX_output = (MUX_tgt == 2'b00) ? mem_out :
+    wire [15:0] MUX_output;
+    assign MUX_output = (MUX_tgt == 2'b00) ? mem_out :
                                (MUX_tgt == 2'b01) ? alu_out :
-                               (MUX_tgt == 2'b10) ? pc + 16'd1;
+                               (MUX_tgt == 2'b10) ? pc + 16'd1:
+                                16'h0000;
+    
+    integer i; // loop in verilog
+    initial begin
+        for (i = 0; i < 8; i = i + 1) register_file[i] = 16'h0000;
+    end
 
     always @(posedge clk) begin
-        if (WE_rf && (rA != 3'b0)) begin //r0 has to stay 0
+        register_file[0] <= 16'h0000; //r0 has to stay 0
+        if (WE_rf && (rA != 3'b0)) begin 
             register_file[rA] <= MUX_output;
         end
     end
