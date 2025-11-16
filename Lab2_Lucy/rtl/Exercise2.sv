4467875a917cc759fadde6eabb6c3e8b9d649f68
Lucy Zheng <luchzheng02005@gmail.com>
2025-11-16 13:36:01 -0500

    lab2-Lucy

---
+++ Lab2_Lucy/rtl/Exercise2.sv
@@ -0,0 +1,28 @@
+/**
+  @brief A fibonacci linear feedback shift register module
+
+  @input clk    clock
+  @input nReset active-low reset
+  @input init   initial value following a reset
+  @output out   current output
+*/
+module Exercise2 (
+    input clk,
+    input nReset,
+    input [15:0] init,
+    output logic [15:0] out
+);
+
+wire feedback_bit;
+assign feedback_bit = out[15] ^ out[13] ^ out[12] ^ out[10];
+
+always @(posedge clk) begin
+    if (nReset) begin
+        out <= init;
+    end
+    else begin
+        out <= {feedback_bit, out[15:1]};
+    end
+end
+
+endmodule
